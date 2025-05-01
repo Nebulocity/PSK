f5b37ca45058c902acd6bac004eb203d261459eb
Nebulocity <Nebulocity@users.noreply.github.com>
2025-05-01 09:00:29 -0700

    Update core.lua
    updated class colors

--- core.lua
+++ core.lua
@@ -17,14 +17,10 @@ local CLASS_COLORS = RAID_CLASS_COLORS or {
     HUNTER =  { r = 0.67, g = 0.83, b = 0.45 },
     ROGUE =   { r = 1.00, g = 0.96, b = 0.41 },
     PRIEST =  { r = 1.00, g = 1.00, b = 1.00 },
-    DEATHKNIGHT = { r = 0.77, g = 0.12, b = 0.23 },
     SHAMAN =  { r = 0.00, g = 0.44, b = 0.87 },
     MAGE =    { r = 0.25, g = 0.78, b = 0.92 },
     WARLOCK = { r = 0.53, g = 0.53, b = 0.93 },
-    MONK =    { r = 0.00, g = 1.00, b = 0.59 },
     DRUID =   { r = 1.00, g = 0.49, b = 0.04 },
-    DEMONHUNTER = { r = 0.64, g = 0.19, b = 0.79 },
-    EVOKER =  { r = 0.20, g = 0.85, b = 0.67 },
 }
 
 
